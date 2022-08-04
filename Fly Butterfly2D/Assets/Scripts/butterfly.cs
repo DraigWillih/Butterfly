76c88e7e27c66fb4a8daf287878efbe19c53c5e1
Andre Luiz Oliveira <andreluiz.pradodeoliveira@gmail.com>
2022-08-04 11:51:45 -0300

    Ajustes das Cenas e UI

--- Fly Butterfly2D/Assets/Scripts/butterfly.cs
+++ Fly Butterfly2D/Assets/Scripts/butterfly.cs
@@ -8,15 +8,24 @@ public class butterfly : MonoBehaviour
     public float speed = 1f;
     private Rigidbody2D rig;
 
-    public GameObject GameOver;
+    public GameObject GameOver, textNectar;
     public TMP_Text nectarText;
+
+    public AudioClip[] soundFx;
+    private AudioSource audioSfx;
 
     // Start is called before the first frame update
     void Start()
     {
+        GameOver = GameObject.FindWithTag("GameOver");
+        textNectar = GameObject.Find("TMPNectar");
+        nectarText = textNectar.GetComponent<TMP_Text>();
+        GameOver.SetActive(false);
         rig = GetComponent<Rigidbody2D>();
         GameController.instance.nectarText = nectarText;
         GameController.instance.StartMission();
+        audioSfx = GetComponent<AudioSource>();
+
     }
 
     // Update is called once per frame
@@ -34,12 +43,16 @@ public class butterfly : MonoBehaviour
         Time.timeScale = 0;
         Timer.stopTime = true;
         GameController.instance.data.Save(GameController.instance.nectar_current, GameController.instance.score_current);
+        audioSfx.clip = soundFx[0];
+        audioSfx.Play();
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         GameController.instance.nectar_current++;
         GameController.instance.nectarText.text = GameController.instance.nectar_current.ToString();
         Destroy(collision.gameObject);
+        audioSfx.clip = soundFx[1];
+        audioSfx.Play();
     }
 
     public void LoadScenes(string cena)
